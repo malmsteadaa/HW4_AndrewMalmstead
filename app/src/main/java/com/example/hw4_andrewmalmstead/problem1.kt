23a3f822d2b9fff635f213828e26355bab0cf1bb
malmsteadaa <malmsteadaa@gmail.com>
2021-11-17 12:01:34 -0800

    k

--- app/src/main/java/com/example/hw4_andrewmalmstead/problem1.kt
+++ app/src/main/java/com/example/hw4_andrewmalmstead/problem1.kt
@@ -1,4 +1,10 @@
 package com.example.hw4_andrewmalmstead
 
 class problem1 {
+    var stacksize=100
+    var buffer= arrayOf<int>(stacksize*3)
+    var stackpointer=[-1,-1,-1]
+fun push(var){
+
+}
 }
