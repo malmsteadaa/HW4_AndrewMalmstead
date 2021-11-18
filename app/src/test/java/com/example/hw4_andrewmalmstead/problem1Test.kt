afbc5887407fa3c1877872298f07fb37fb77e441
Andrew <malmsteadaa@gmail.com>
2021-11-18 11:52:56 -0800

    adding test

--- app/src/test/java/com/example/hw4_andrewmalmstead/problem1Test.kt
+++ app/src/test/java/com/example/hw4_andrewmalmstead/problem1Test.kt
@@ -3,10 +3,22 @@ package com.example.hw4_andrewmalmstead
 import junit.framework.TestCase
 
 class problem1Test : TestCase() {
+    //purpose can we get stack size initially at 100
+    fun testGetStacksize() {
+        val myClass=problem1()
+        assertEquals(100,myClass.stacksize)
+    }
+//purpose can we change the stack size
+    fun testSetStacksize() {
+    val myClass=problem1()
+    //re-init buffer
+    myClass.buffer= arrayOf<Int>(200*3)
+    //set the size of each stack
+        myClass.stacksize=200
+    assertEquals(200,myClass.stacksize)
+    assertEquals(600,myClass.buffer.size)
 
-    fun testGetStacksize() {}
-
-    fun testSetStacksize() {}
+    }
 
     fun testGetBuffer() {}
 
