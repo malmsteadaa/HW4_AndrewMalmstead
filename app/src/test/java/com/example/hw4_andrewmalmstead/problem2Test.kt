1afa6c59f492fb0d6e19cf77528ab23ee1aeeee9
Andrew <malmsteadaa@gmail.com>
2021-11-19 01:48:45 -0800

    State Purpose and build general test functions.

--- app/src/test/java/com/example/hw4_andrewmalmstead/problem2Test.kt
+++ app/src/test/java/com/example/hw4_andrewmalmstead/problem2Test.kt
@@ -4,7 +4,24 @@ import junit.framework.TestCase
 
 class problem2Test : TestCase() {
 
-    fun testSearchR() {}
-
-    fun testSearch() {}
+    //Purpose 1:backwards array
+    fun testBackwards(){}
+    //Purpose 2:sorted array with spaces
+    fun testSpace(){}
+    //Purpose 3:sorted numbers and letters array.
+    fun testNumberLetters(){}
+    //Purpose 4:sorted numbers
+    fun testNumbers(){}
+    //Purpose 5:sorted letters
+    fun testLetters(){}
+    //Purpose 6:sorted words
+    fun testWords(){}
+    //Purpose 7:-1 case when there is an empty array
+    fun testEmptyArray(){}
+    //Purpose 8:-1 case when the element is empty
+    fun testEmptyElement(){}
+    //Purpose 9:-1 case when element is not the array.
+    fun testNotPresent(){}
+    //Purpose 10:Unsorted list. it doesn't have to be sorted for worse case where it take O(N) operations.
+    fun testUnsorted(){}
 }
